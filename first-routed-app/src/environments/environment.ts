e1d1cbe75f3c7db40fbb673f6f9767dd3a203f3b
LongoL4 <longo.luca@iisgalvanimi.edu.it>
2022-04-05 11:51:20 +0000

    Progg spotify

--- first-routed-app/src/environments/environment.ts
+++ first-routed-app/src/environments/environment.ts
@@ -1,16 +1,4 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  oauthToken: "Bearer BQBwx7teR5nIWcNvKY2O3gKpmoouZt8jQgXHV7SXaz4CyDttd3lb64mYpq6xuNttvqpAduW7mxaUuXpoYswwHFC9d6TQSABGbSHjG1pKzKLajLu7-t5i9q124TJ8J-G2hMFoiIIaGADiG-kMgQ"
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
